7af2054cac638595bc85afa8927bcef73c56292f
MarcinLes7 <Gitm@rtino7>
2023-06-11 08:47:08 +0200

    Initial commit

---
+++ src/main/java/pl/edu/wszib/jwd/oemdatabase/OemDataBaseApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.jwd.oemdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OemDataBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OemDataBaseApplication.class, args);
+    }
+
+}
